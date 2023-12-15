03c8d381a97e62f85b4ba4e0c2ecbfb64f4c67dd
arvnddevops <arvnddevops@gmail.com>
2023-12-15 12:50:57 +0530

    Inti

---
+++ java/com/example/tictactoe/TicTacToe.java
@@ -0,0 +1,43 @@
+package com.example.tictactoe;
+
+public class TicTacToe {
+    private char[][] board;
+    private char currentPlayerMark;
+
+    public TicTacToe() {
+        board = new char[3][3];
+        currentPlayerMark = 'X';
+        initializeBoard();
+    }
+
+    public void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    public boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean checkForWin() {
+        // Check rows, columns, and diagonals for a win
+        // Logic to check for a win, return true if found, false otherwise
+    }
+
+    public void changePlayer() {
+        currentPlayerMark = (currentPlayerMark == 'X') ? 'O' : 'X';
+    }
+
+    // Other methods for handling player moves, displaying the board, etc.
+}
+
